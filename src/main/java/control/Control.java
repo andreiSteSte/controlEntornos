73a12aecbf2d8205d5ae1116586fc42a3e63e0b4
andreiSteSte <andreistefanstefan1997@gmail.com>
2023-02-17 12:16:23 +0100

    Se ha Realizado un Commit

--- src/main/java/control/Control.java
+++ src/main/java/control/Control.java
@@ -1,2 +1,26 @@
-package control;public class Control {
+package control;
+
+public class Control {
+
+    public static double ej9(char tipo, double sueldo, int antiguedad) {
+        double incremento;
+        switch(tipo) {
+            case 'E': incremento=sueldo*1.0;
+            case 'N': incremento=sueldo*0.5;
+            default: incremento=0.0;
+        }
+        if (antiguedad>24)
+            incremento=incremento*antiguedad/10;
+        else
+        if (antiguedad>12)
+            incremento=incremento*antiguedad/20;
+        else
+            incremento=incremento*antiguedad/30;
+        return sueldo+incremento; // línea donde establecer un punto de ruptura, breakpoint
+    }
+
+    public static void main(String[] args) {
+        ej9('N',1000,14 );
+    }
+    //Primer Commit
 }
--- src/test/java/control/ControlTest.java
+++ src/test/java/control/ControlTest.java
@@ -1,4 +1,36 @@
+package control;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ControlTest {
-  
+
+    @Test
+    void ej91() {
+        assertEquals(1033, Control.ej9('N',1000,1));
+        //No se incrementa el sueldo en la proporcion necesaria, el incremento nunca llega a ocurrir
+    }
+    @Test
+    void ej92() {
+        assertEquals(1016, Control.ej9('E',1001,1));
+        //No se realiza el incremento
+    }
+    @Test
+    void ej93() {
+
+        assertEquals(1000, Control.ej9('R',1000,1));
+
+    }
+    @Test
+    void ej94() {
+
+        assertEquals(1000, Control.ej9('N',1000,0));
+
+    }
+    @Test
+    void ej95() {
+
+        assertEquals(1000, Control.ej9('N',999,1));
+    }
 }
